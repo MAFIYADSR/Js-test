da48b0032dcb7da174a55f76fd2da999685d9bf5
Akash dutta <104191461+MAFIYADSR@users.noreply.github.com>
2023-06-29 21:41:50 +0530

    Update tudu.js

--- tudu.js
+++ tudu.js
@@ -1,57 +1,82 @@
-function tudu(event) {
+function test(event) {
     event.preventDefault();
-    const todoname = document.getElementById('todoname').value;
+    const todo = document.getElementById('todo').value;
     const description = document.getElementById('description').value;
 
-    axios.post("https://crudcrud.com/api/590bf275b97c47c5afa423708b7b8a03/whatToDo", obj)
+
+    // var user = localStorage.setItem("username", username);
+    // var em = localStorage.setItem("email", email);
+    // var pass = localStorage.setItem("password", password);
+
+    const obj = {
+        todo,
+        description
+    }
+    // localStorage.setItem(obj.email, JSON.stringify(obj));
+    axios.post("https://crudcrud.com/api/f4bae4ce1893414897a071ac3aee314a/tuDo", obj)
         .then((response) => {
-            showUserOnscreen(response.data)
-            console.log(response)
+            showUserOnScreen(response.data);
+            console.log(response);
         })
         .catch((err) => {
-            console.log(err)
+            console.log(err);
         })
+    // showUserOnScreen(obj);
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-    axios.get("https://crudcrud.com/api/590bf275b97c47c5afa423708b7b8a03/whatToDo")
+    axios.get("https://crudcrud.com/api/f4bae4ce1893414897a071ac3aee314a/tuDo")
         .then((response) => {
-            console.log(response)
-            for(var i = 0; i<response.data.length; i++)
-            {
-                showUserOnscreen(response.data[i])
+            console.log(response);
+            for (var i = 0; i < response.data.length; i++) {
+                showUserOnScreen(response.data[i]);
             }
         })
-        .catch((error) => {
-            console.log(error)
+        .catch((err) => {
+            console.log(err);
         })
+
 })
+function showUserOnScreen(obj) {
+    // document.getElementById('username').value = '';
+    // document.getElementById('email').value = '';
+    // document.getElementById('password').value = '';
 
-function showUserOnscreen(obj){
-    const parentElem = document.getElementById('listofitems');
-    const childElem = `<li id= ${obj._id}> ${obj.todoname}- ${obj.description}
-                          <button onclick=deleteUser('${obj._id}')> DeleteUser</button>
-                        </li>`
+    const parentNode = document.getElementById('listofitems');
+    const childHTML = `<li id= ${obj._id}> ${obj.todo} - ${obj.description}
+                            <button onclick= deleteUser('${obj._id}')> Delete User </button>
+                        </li>`;
 
-    parentElem.innerHTML = parentElem.innerHTML + childElem;
+    parentNode.innerHTML = parentNode.innerHTML + childHTML;
 
-    function deleteUser(userId)
-    {
-        axios.delete(`https://crudcrud.com/api/590bf275b97c47c5afa423708b7b8a03/whatToDo/${userId}`)
-            .then((response) => {
-                removeUserFromScreen(userId)
-            })
-            .catch((err) => {
-                console.log(err)
-            })
-    }
+}
 
-    function removeUserFromScreen(userId)
-    {
-        const parentElem = document.getElementById('listofitems')
-        const childNodeToBeDeleted = document.getElementById(userId);
-        if (childNodeToBeDeleted) {
-            parentElem.removeChild(childNodeToBeDeleted)
-        }
+function deleteUser(userId)
+{
+    axios.delete(`https://crudcrud.com/api/f4bae4ce1893414897a071ac3aee314a/tuDo/${userId}`)
+        .then((response) => {
+            removeUserFromScreen(userId)
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+}
+
+// function editUser(todo, description, userId)
+// {
+//         document.getElementById('todo').value = todo;
+//         document.getElementById('description').value =description;
+
+//         deleteUser(userId);
+// }
+
+function removeUserFromScreen(userId)
+{
+    const parentNode = document.getElementById('listofitems');
+    const childNodeToBeDeleted = document.getElementById(userId);
+    if (childNodeToBeDeleted) {
+        parentNode.removeChild(childNodeToBeDeleted);
     }
 }
+
+// {/* <button onclick= editUser('${obj.todo}','${obj.description}','${obj._id}')> Edit User </button> */}
